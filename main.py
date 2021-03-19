aaf6818b359dfd518075b2166d6416f32facdbc1
Yhoundeh <aarond96@byui.edu>
2021-03-19 14:01:59 -0600

    AD id thoughts

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
 SCREEN_TITLE = 'Historical Pursuit'
 ROW_COUNT = 6
-COLUMN_COUNT = 6
+COLUMN_COUNT = 5
 GRID_WIDTH = 100
 GRID_HEIGHT = 100
 MARGIN = 5
@@ -19,16 +19,17 @@ class MyFlatButton(arcade.gui.UIFlatButton):
     """
     To capture a button click, subclass the button and override on_click.
     """
-    def __init__(self, text, center_x, center_y, width, height, window):
+    def __init__(self, text, center_x, center_y, width, height, window, myid):
+        self.myid = myid
         super().__init__(text, center_x, center_y, width, height)
         self.window = window
 
 
-    def on_click(self):
+    def on_click(self, myid):
         """ Called when user lets off button """
-        question_view = QuestionView()
+        question_view = QuestionView(myid)
         self.window.show_view(question_view)
-        active_view = 'question_view'
+        #active_view = 'question_view'
 
 class MenuView(arcade.View):
     """ Class that manages the 'menu' view. """
@@ -76,10 +77,12 @@ class GameView(arcade.View):
 
     def setup(self):
         self.ui_manager.purge_ui_elements()
+        question = 0
         for row in range(ROW_COUNT):
             for column in range(COLUMN_COUNT):
-                right_column_x = column * (GRID_WIDTH + MARGIN) + (GRID_WIDTH*4 + MARGIN)
-                y_slot = row * (GRID_HEIGHT + MARGIN) + (GRID_HEIGHT / 1 + MARGIN)
+                question += 1
+                column_x = column * (GRID_WIDTH + MARGIN) + (GRID_WIDTH*5 + MARGIN)
+                row_y = row * (GRID_HEIGHT + MARGIN) + (GRID_HEIGHT / 1 + MARGIN)
                 if row < 2:
                     points = '100'
                 elif row < 4:
@@ -88,20 +91,17 @@ class GameView(arcade.View):
                     points = '500'
                 else:
                     points = 'error'
+                    #https://www.geeksforgeeks.org/g-fact-42-changing-class-members-python/
                 button = MyFlatButton(
-                text = points,
-                center_x=right_column_x,
-                center_y=y_slot * 1,
-                width=100,
-                height=100,
-                window = self.window)
+                text = str(question) + '\n'+ points,
+                center_x = column_x,
+                center_y = row_y,
+                width = 100,
+                height = 100,
+                window = self.window,
+                myid = question
+                )
                 self.ui_manager.add_ui_element(button)
-
-        # left button array
-       
-
-        
-            
 
     def on_key_press(self, key, _modifiers):
         #if space key is pressed this shows the question view
@@ -110,15 +110,15 @@ class GameView(arcade.View):
             self.window.show_view(question_view)
             active_view = 'question_view'
 
-class QuestionView(arcade.View):
+class QuestionView(arcade.View, myid):
     def __init__(self):
         super().__init__()
         arcade.set_background_color(arcade.color.BABY_BLUE)
 
-    def on_draw(self):
+    def on_draw(self, myid):
         """ Draws the question view """
         arcade.start_render()
-        arcade.draw_text("Questions Will Go Here", SCREEN_WIDTH/2, SCREEN_HEIGHT/2, arcade.color.BLACK, 30, anchor_x="center")
+        arcade.draw_text("Questions Will Go Here" + myid, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, arcade.color.BLACK, 30, anchor_x="center")
 
     def on_key_press(self, key, _modifiers):
         """ If user hits escape, go back to the game view """
